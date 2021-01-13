05781028f0c33285eb59f8443f4f482eeae1b020
Hdhillon0001 <53787323+Hdhillon0001@users.noreply.github.com>
2021-01-13 02:05:23 -0600

    basic car simulator with different assets
    car simulator with basic controller , with the abilty to fly .
    
    experimetns with terrain and trees .
    
    feel free to  to do crazy stuff with it

---
+++ New Unity Project/Assets/Scripts/Controller.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public float carSpeed = 12;
+    public float turnAngle = 02/10;
+    public  Vector3 cameraoffset = new Vector3(0, 6, -10);
+    public new Camera camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Game start");
+    }
+    /*
+    public Transform pivot;
+    public float speed;
+    public Vector3 axis;
+
+    void Update()
+    {
+        transform.RotateAround(pivot.position, axis, speed * Time.deltaTime);
+    }*/
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        camera.transform.position = transform.position + cameraoffset ;
+        if (Input.GetButton("Liftdown"))
+        {
+            transform.Rotate(-1, 0, 0);
+        }
+        // movement of car
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward*Time.fixedDeltaTime*carSpeed);
+            //camera.transform.position = transform.position + cameraoffset;
+            if (Input.GetButton("Liftoff"))
+            {
+                transform.Rotate(2, 0, 0);
+            }
+
+
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.forward*Time.fixedDeltaTime*-carSpeed);
+            //camera.transform.position = transform.position + cameraoffset;
+        }
+        // rotation of car
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(0,-turnAngle , 0);
+           // camera.transform.Rotate(new Vector3(0, -turnAngle, 0) + transform.Rotate(0,0,0), Time.deltaTime) ; //transform.Rotate(0,-turnAngle,0) 
+        }
+
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(0, turnAngle, 0);
+        }
+    }
+}
---
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 12.0f;
+    public float speedTurn = 6.1f;
+    public new Camera camera;
+    public Vector3 offset = new Vector3(0, 5,-7);
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        //Move the car foward
+       // transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        camera.transform.position = transform.position + offset;
+        //car controller
+
+        if (Input.GetKey(KeyCode.UpArrow))
+            {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                //transform.Rotate(0, speedTurn, 0);
+                transform.Rotate(Vector3.right, speedTurn);
+
+                camera.transform.Rotate(0, 1/10, 0)  ;
+                ;
+
+                // transform.Translate(Vector3.right * Time.deltaTime * speedTurn);
+            }
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                // transform.Rotate(0, -speedTurn, 0);
+                transform.Rotate(Vector3.right,-speedTurn);
+                camera.transform.Rotate(0, -1/10, 0);
+
+                //  transform.Translate(Vector3.left * Time.deltaTime * speedTurn);
+            }
+        }
+         if (Input.GetKey(KeyCode.DownArrow)) 
+           {
+            transform.Translate(Vector3.back * Time.deltaTime * speed);
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.Rotate(0, speedTurn, 0);
+
+                // transform.Translate(Vector3.right * Time.deltaTime * speedTurn);
+            }
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.Rotate(0, -speedTurn, 0);
+                //  transform.Translate(Vector3.left * Time.deltaTime * speedTurn);
+            }
+        }
+
+        
+    }
+}
